6da29c66e3cd66e31ce7990b404d50a59527e350
Nelson_Valle <nelson.valle@udea.edu.co>
2022-10-18 21:08:32 -0500

    Segundo commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app= Flask(__name__)
 def inicio():
     return render_template('sitio/index.html')
 
-@app.route('/libros')
+@app.route('/libros')   
 def libros():
     return render_template('sitio/libros.html')
 
@@ -17,13 +17,17 @@ def nosotros():
     return render_template('sitio/nosotros.html')
 
 
-@app.route('/admin')
+@app.route('/admin/')
 def admin_index():
     return render_template('admin/index.html')
 
 @app.route('/admin/login')
 def admin_login():
     return render_template('admin/login.html')
+
+@app.route('/admin/libros')
+def admin_login():
+    return render_template('admin/libros.html')
 
 
 if __name__ =='__main__':
